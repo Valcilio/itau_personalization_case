1864f8b6f7595b3630c95f6967dd6a24e8bba2a3
valcilio <eugeniovalcilio@gmail.com>
2026-07-30 09:33:18 -0300

    feat: testing terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,7 +5,7 @@ terraform {
     bucket         = "personalization-terraform-state-272175292064"
     key            = "terraform/state/model-train/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile = true
+    dynamodb_table = "personalization-terraform-locks"
     encrypt        = true
   }
 
